63691d1794ebc30ce9a64ff68660b02a6e627630
ianzapolsky <ianzapolsky@gmail.com>
2015-02-13 20:10:50 +0000

    increased buffer size. changed location

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var gith = require('gith').create(4000);
 var exec = require('child_process').execFile;
 
+var execOptions = {
+     maxBuffer: 1024 * 1024 // 1mb
+}
+
 gith({repo: 'ianzapolsky/alumnus'}).on('all', function( payload ) {
 
   if (payload.branch === 'master') {
     // Exec a shell script
-    exec('/Users/ianzapolsky/projects/webhooks-server/hello.sh', function(error, stdout, stderr) {
+    exec('/home/ubuntu/alumnus-django/replace.sh', execOptions, function(error, stdout, stderr) {
       // Log success in some manner
       console.log( 'exec complete' );
     });
