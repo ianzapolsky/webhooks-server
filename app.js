34df82eba019c83a7b9ce796296ab4577f8d141c
ianzapolsky <ianzapolsky@gmail.com>
2015-02-14 15:13:30 +0000

    added working webhooks server made with express

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-var gith = require('gith').create(4000);
-var exec = require('child_process').execFile;
+var _     = require('underscore');
+var app   = require('express')();
+var spawn = require('child_process').spawn;
+var bodyParser = require('body-parser');
 
-var execOptions = {
-     maxBuffer: 1024 * 1024 // 1mb
-}
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-gith({repo: 'ianzapolsky/alumnus'}).on('all', function( payload ) {
-
-  if (payload.branch === 'master') {
-    // Exec a shell script
-    exec('/home/ubuntu/alumnus-django/replace.sh', execOptions, function(error, stdout, stderr) {
-      // Log success in some manner
-      console.log( 'exec complete' );
+app.post('/', function(req, res){
+  if (req.body.ref === 'refs/heads/master') {
+    // Exec deploy script
+    var deploySh = spawn('sh', [ 'replace.sh' ], {
+      cwd: '/home/ubuntu/alumnus-django',
+      env:_.extend(process.env, { PATH: process.env.PATH + ':/usr/local/bin' })
     });
   }
+  res.send('hello world');
 });
+
+app.listen(4001);
+
